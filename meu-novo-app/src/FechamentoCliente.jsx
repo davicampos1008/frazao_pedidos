355629a880e257c90b988adb61c72a56fd7f4302
Davi Campos <nygames92@gmail.com>
2026-03-11 20:26:04 -0300

    ar

---
+++ meu-novo-app/src/FechamentoCliente.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function FechamentoCliente() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h2>Tela de Fechamento do Cliente</h2>
+      <p>Em desenvolvimento...</p>
+    </div>
+  );
+}
